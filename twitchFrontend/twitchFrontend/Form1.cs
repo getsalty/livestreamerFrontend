64c76d8b26a1d3dc0faf863ab11b8ac1d86e8894
Garrett Long <gblz72@mail.umsl.edu>
2016-03-01 09:10:05 -0800

    Moved service calls into their own folder/class

--- twitchFrontend/twitchFrontend/Form1.cs
+++ twitchFrontend/twitchFrontend/Form1.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using twitch.Models;
+using twitchFrontend.Service;
 
 namespace twitchFrontend
 {
     public partial class Form1 : Form
     {
         List<Streamer> _datasource = new List<Streamer>();
+        AppService _serviceHandler;
 
         public Form1()
         {
@@ -21,146 +19,16 @@ namespace twitchFrontend
             _datasource.Add(new Streamer { name = "bye", url = "/sss", online = false });
             _datasource.Add(new Streamer { name = "hello", url = "/stream2", online = false });
 
+
             dataGridView1.DataSource = _datasource;
+            _serviceHandler = new AppService(_datasource);
 
             //Task llamaq = RunAsync2();
-            Task.Run(() => RunAsync()).Wait();
-        }
 
-
-        async Task RunAsync2()
-        {
-            var time = new Stopwatch();
-            time.Start();
-
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://localhost:3000");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                try
-                {
-                    dataGridView1.SuspendLayout();
-                    foreach (var item in _datasource)
-                    {
-                        HttpResponseMessage response = await client.GetAsync(item.url);
-                        if (response.IsSuccessStatusCode)
-                        {
-                            var product = await response.Content.ReadAsAsync<TEST>();
-                            if (product.Name == item.name)
-                                item.online = true;
-                        }
-                        else
-                        {
-                            Debug.WriteLine("Dangit");
-                        }
-                    }
-                    dataGridView1.ResumeLayout();
-                    dataGridView1.Refresh();
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine(e.InnerException);
-                }
-            }
-
-            time.Stop();
-            var ts = time.Elapsed;
-            var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            Debug.WriteLine("RunTime " + elapsedTime);
-        }
-
-        async Task RunAsync()
-        {
-            var time2 = new Stopwatch();
-            time2.Start();
-
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://localhost:3000");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                try
-                {
-                    dataGridView1.SuspendLayout();
-
-                    ////
-                    //    PARALLEL PROCESSING
-                    ////
-                    var tasks = new List<Task>();
-                    foreach (var item in _datasource)
-                    {
-                        tasks.Add(RetrieveStreamInfo(client, item));
-                    }
-                    await Task.WhenAll(tasks);
-                    dataGridView1.ResumeLayout();
-                    dataGridView1.Refresh();
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine(e.InnerException);
-                }
-            }
-
-            time2.Stop();
-            var ts = time2.Elapsed;
-            var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            Debug.WriteLine("RunTime for ASYNC1 " + elapsedTime);
-        }
-
-        async Task RetrieveStreamInfo(HttpClient client, Streamer item)
-        {
-
-            HttpResponseMessage response = await client.GetAsync(item.url);
-            if (response.IsSuccessStatusCode)
-            {
-                var product = await response.Content.ReadAsAsync<TEST>();
-                if (product.Name == item.name)
-                    item.online = true;
-                //var product = await response.Content.ReadAsAsync<Stream>();
-                //if (product.channel.name == item.name)
-                //    item.online = true;
-            }
-            else
-            {
-                Debug.WriteLine("Dangit");
-
-                //this.pictureBox1.Image = global::twitchFrontend.Properties.Resources.red;
-            }
-
-        }
-        async Task RunAsync22()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("http://localhost:3000");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                try
-                {
-                    HttpResponseMessage response = await client.GetAsync("/stream");
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var product = await response.Content.ReadAsAsync<TEST>();
-                        Console.WriteLine("Got it");
-                    }
-                    else
-                    {
-                        Console.WriteLine("gg");
-                    }
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine(e.InnerException);
-                }
-            }
+            dataGridView1.SuspendLayout();
+            Task.Run(() => _serviceHandler.UpdateStreamers()).Wait();
+            dataGridView1.ResumeLayout();
+            dataGridView1.Refresh();
         }
 
         void cellFormattingImage(object sender, DataGridViewCellFormattingEventArgs e)
@@ -170,24 +38,6 @@ namespace twitchFrontend
             {
                 e.Value = (bool)e.Value ? (System.Drawing.Image)Properties.Resources.green : (System.Drawing.Image)Properties.Resources.red;
                 e.FormattingApplied = true;
-            }
-        }
-
-        class TEST
-        {
-            string name;
-
-            public string Name
-            {
-                get
-                {
-                    return name;
-                }
-
-                set
-                {
-                    name = value;
-                }
             }
         }
     }
---
+++ twitchFrontend/twitchFrontend/Service/AppService.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using twitch.Models;
+
+namespace twitchFrontend.Service
+{
+    class AppService
+    {
+        List<Streamer> _datasourceStreamer;
+
+        public AppService(List<Streamer> _datasource)
+        {
+            _datasourceStreamer = _datasource;
+        }
+
+        public async Task UpdateStreamers()
+        {
+            var time2 = new Stopwatch();
+            time2.Start();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    ////
+                    //    PARALLEL PROCESSING
+                    ////
+                    var tasks = new List<Task>();
+                    foreach (var item in _datasourceStreamer)
+                    {
+                        tasks.Add(RetrieveStreamInfo(client, item));
+                    }
+                    await Task.WhenAll(tasks);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.InnerException);
+                }
+            }
+
+            time2.Stop();
+            var ts = time2.Elapsed;
+            var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Debug.WriteLine("RunTime for ASYNC1 " + elapsedTime);
+        }
+
+        async Task RetrieveStreamInfo(HttpClient client, Streamer item)
+        {
+            HttpResponseMessage response = await client.GetAsync(item.url);
+            if (response.IsSuccessStatusCode)
+            {
+                var product = await response.Content.ReadAsAsync<TEST>();
+                if (product.Name == item.name)
+                    item.online = true;
+                //var product = await response.Content.ReadAsAsync<Stream>();
+                //if (product.channel.name == item.name)
+                //    item.online = true;
+            }
+            else
+            {
+                Debug.WriteLine("Dangit");
+
+                //this.pictureBox1.Image = global::twitchFrontend.Properties.Resources.red;
+            }
+        }
+
+        class TEST
+        {
+            string name;
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+
+                set
+                {
+                    name = value;
+                }
+            }
+        }
+
+
+
+        //----------------------------------------------------
+
+
+        async Task RunAsync2()
+        {
+            var time = new Stopwatch();
+            time.Start();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    foreach (var item in _datasourceStreamer)
+                    {
+                        HttpResponseMessage response = await client.GetAsync(item.url);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var product = await response.Content.ReadAsAsync<TEST>();
+                            if (product.Name == item.name)
+                                item.online = true;
+                        }
+                        else
+                        {
+                            Debug.WriteLine("Dangit");
+                        }
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.InnerException);
+                }
+            }
+
+            time.Stop();
+            var ts = time.Elapsed;
+            var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Debug.WriteLine("RunTime " + elapsedTime);
+        }
+
+        async Task RunAsync22()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("/stream");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var product = await response.Content.ReadAsAsync<TEST>();
+                        Console.WriteLine("Got it");
+                    }
+                    else
+                    {
+                        Console.WriteLine("gg");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.InnerException);
+                }
+            }
+        }
+    }
+}
